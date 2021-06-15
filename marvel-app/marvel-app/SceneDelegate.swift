b9c6bd0f7ecabb65ade2e3884162d01a3ac68630
Ruben Marquez <rubenmfdeveloper@gmail.com>
2021-06-15 18:17:51 +0200

    NEW: Add SOLID architecture
    #3

--- marvel-app/marvel-app/SceneDelegate.swift
+++ marvel-app/marvel-app/SceneDelegate.swift
@@ -13,10 +13,23 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+
+        // Get the scene if there is one into a variable
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        // Create a new UIWindow using the windowScene constructor which takes in a window scene
+        let window = UIWindow(windowScene: windowScene)
+        
+        // Create the view hierarchy
+        let viewController = ViewController()
+        let navigationController = UINavigationController(rootViewController: viewController)
+        
+        // Set the root view controller of the window with the view controller
+        window.rootViewController = navigationController
+        
+        // Set the window and make it keyAndVisible
+        self.window = window
+        window.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
